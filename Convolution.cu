9da8bfb29eec3ce6770ac627aefa9f05251ea060
max <max.maxkot@gmail.com>
2020-03-11 19:21:25 -0400

    cuda memcopy. passing output vector into kernel now.

--- Convolution.cu
+++ Convolution.cu
@@ -4,9 +4,19 @@
 #include <functional>
 #include <cuda.h>
 
-struct Startup{
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
-};
+typedef struct Startup{
+    unsigned int threadsperblock = 1024;
+} startup;
 
 struct Result {
     float executiontime;
@@ -19,19 +29,31 @@ struct Result {
     }
 };
 
-//__global__ NaiveConvolution(int* data, int datasize, int* kernel, int* kernelsize){
-//
-//
-//}
+__global__ void NaiveConvolution(int* data, int dataSize, int* kernel, int kernelSize, int* output, int outputSize){
 
-bool Symmetric(const std::vector<int>& vec){
+}
+
+__global__ void ConstantConvolution(int* data, int dataSize, int* kernel, int kernelSize, int* output, int outputSize){
+    
+}
+
+__global__ void SharedConvolution(int* data, int dataSize, int* kernel, int kernelSize, int* output, int outputSize){
+    
+}
+
+typedef std::function<void(int*,int,int*,int,int*,int)> ConvolutionCudaKernel;
+std::vector<ConvolutionCudaKernel> cudaKernels{ 
+    NaiveConvolution, ConstantConvolution, SharedConvolution 
+};
+
+bool isSymmetric(const std::vector<int>& vec){
     for(int i = 0; i < vec.size()/2; i++)
         if (vec[i] != vec[vec.size()-1-i])
             return false;
     return true;
 }
 
-int CalculateOutputSize(inputsize, kernelsize){
+int CalculateOutputSize(int inputsize, int kernelsize){
     kernelsize /= 2;
     return inputsize+ 2*kernelsize;
 }
@@ -39,7 +61,7 @@ int CalculateOutputSize(inputsize, kernelsize){
 Result CpuPerformConvolution(const std::vector<int>& input, const std::vector<int>& kernel){
     std::vector<int> output(CalculateOutputSize(input.size(), kernel.size()));
 
-    bool isKernelSymmetric = Symmetric(kernel);
+    bool isKernelSymmetric = isSymmetric(kernel);
     for (int i = 0; i < input.size(); i++){
         if (isKernelSymmetric){
             for (int k = 0; k < kernel.size()/2+1; k++){
@@ -57,7 +79,21 @@ Result CpuPerformConvolution(const std::vector<int>& input, const std::vector<in
     return std::move(r);
 }
 
-Result CudaPerformConvolution(const std::vector<int>& input, const std::vector<int>& kernel){
+Result CudaPerformConvolution(const std::vector<int>& input, const std::vector<int>& kernel, ConvolutionCudaKernel cudakernel){
+    int* device_input, *device_kernel, *device_output;
+    std::vector<int> output(CalculateOutputSize(input.size(), kernel.size()));
+
+    gpuErrchk(cudaMalloc((void **)&device_input,   input.size()));
+    gpuErrchk(cudaMalloc((void **)&device_kernel, kernel.size()));
+    gpuErrchk(cudaMalloc((void **)&device_output, output.size()));
+
+    gpuErrchk(cudaMemcpy(device_input,   input.data(),  input.size(), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(device_kernel, kernel.data(), kernel.size(), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(device_output, output.data(), output.size(), cudaMemcpyHostToDevice));
+
+    unsigned int threadsneeded = 0;
+    //cudakernel<<<output.size() / threadsperblock+1, threadsperblock>>>(device_input, device_kernel, device_output)
+
     Result r = {0, input};
     return std::move(r);
 }
@@ -67,6 +103,6 @@ int main(int argc, char** argv){
     int inputsize = 1024; int kernelsize = 3;
     std::vector<int> input(inputsize);
     std::generate(input.begin(), input.end(), []() { return rand() % 100; });
-    std::vector<int> kernel{0,1,0};
-    Result r = PerformConvolution(input, kernel);
+    std::vector<int> kernel{1,2,3,2,1};
+    Result r = CpuPerformConvolution(input, kernel);
 }
