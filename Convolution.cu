e97bd022d8fff0a877943bce108d1a975865a71b
max <max.maxkot@gmail.com>
2020-03-12 09:47:04 -0400

    Naive Kernel Implementation should now ouput correct convolution

--- Convolution.cu
+++ Convolution.cu
@@ -24,6 +24,7 @@ struct Result {
 
     /*Move operator to only shallow copy vector*/
     Result& operator=(const Result& other) {
+        executiontime = other.executiontime;
         output = std::move(other.output);
         return *this;
     }
@@ -31,16 +32,27 @@ struct Result {
 
 struct KernelParameters {
     int* input;
-    int inputSize;
-    int* kernel;
-    int kernelSize;
+    int  inputsize;
+    int* filter;
+    int  filtersize;
     int* output;
-    int outputSize;
+    int  outputsize;
+    int  ghostvalue = 0;
 };
 
 __global__ void NaiveConvolution(KernelParameters parameters){
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    
+    int outputindex = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (outputindex < parameters.outputsize){
+        int result = parameters.ghostvalue;
+        int inputstart = outputindex - (parameters.filtersize/2)-1;
+        for (int filterindex = 0; filterindex < parameters.filtersize; filterindex++) {
+            int inputindex = inputstart + filterindex;
+            if (inputindex >= 0 && inputindex < parameters.outputsize)
+                result += parameters.input[inputindex] * parameters.filter[filterindex];
+        }
+        parameters.output[outputindex] = result;
+    }
 }
 
 __global__ void ConstantConvolution(KernelParameters parameters){
@@ -63,9 +75,9 @@ bool isSymmetric(const std::vector<int>& vec){
     return true;
 }
 
+
 int CalculateOutputSize(int inputsize, int filtersize){
-    filtersize /= 2;
-    return inputsize+ 2*filtersize;
+    return inputsize+ filtersize-1;
 }
 
 Result CpuPerformConvolution(const std::vector<int>& input, const std::vector<int>& filter){
@@ -78,10 +90,6 @@ Result CpuPerformConvolution(const std::vector<int>& input, const std::vector<in
                 output[k+i] = filter[k] * input[k+i];
                 output[(filter.size() - k - 1)+i] = filter[k] * input[(filter.size() - k - 1)+i];
             }
-        } else {
-            for (int k = 0; k < filter.size(); k++){
-
-            }
         }
     }
 
@@ -89,49 +97,57 @@ Result CpuPerformConvolution(const std::vector<int>& input, const std::vector<in
     return std::move(r);
 }
 
-Result CudaPerformConvolution(const std::vector<int>& input, const std::vector<int>& kernel, ConvolutionCudaKernel algorithm){
-    int* device_input, *device_kernel, *device_output; Result result;
-    std::vector<int> output(CalculateOutputSize(input.size(), kernel.size()));
+Result CudaPerformConvolution(const std::vector<int>& input, const std::vector<int>& filter, ConvolutionCudaKernel algorithm){
+    int* device_input, *device_filter, *device_output; Result result;
+    std::vector<int> output(CalculateOutputSize(input.size(), filter.size()));
 
     gpuErrchk(cudaMalloc((void **)&device_input,   input.size()*sizeof(int)));
-    gpuErrchk(cudaMalloc((void **)&device_kernel, kernel.size()*sizeof(int)));
+    gpuErrchk(cudaMalloc((void **)&device_filter, filter.size()*sizeof(int)));
     gpuErrchk(cudaMalloc((void **)&device_output, output.size()*sizeof(int)));
 
     gpuErrchk(cudaMemcpy(device_input,   input.data(),  input.size()*sizeof(int), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(device_kernel, kernel.data(), kernel.size()*sizeof(int), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(device_filter, filter.data(), filter.size()*sizeof(int), cudaMemcpyHostToDevice));
     gpuErrchk(cudaMemcpy(device_output, output.data(), output.size()*sizeof(int), cudaMemcpyHostToDevice));
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);    
 
-    KernelParameters parameters = { (int*)device_input, (int)input.size(), (int*)device_kernel, (int)kernel.size(), (int*)device_output, (int)output.size() };
+    KernelParameters parameters = { (int*)device_input, (int)input.size(), (int*)device_filter, (int)filter.size(), (int*)device_output, (int)output.size() };
+    cudaEventRecord(start);
     algorithm<<< output.size() / startup.threadsperblock+1, startup.threadsperblock>>>(parameters);
-
-    cudaEventRecord(stop);
+    gpuErrchk(cudaEventRecord(stop));
+    
     gpuErrchk(cudaEventSynchronize(stop));
-    cudaEventElapsedTime(&result.executiontime, start, stop);
+    gpuErrchk(cudaEventElapsedTime(&result.executiontime, start, stop));
 
     gpuErrchk(cudaMemcpy(output.data(), device_output, output.size()*sizeof(int), cudaMemcpyDeviceToHost));
-    gpuErrchk(cudaFree(device_input)); gpuErrchk(cudaFree(device_kernel)); gpuErrchk(cudaFree(device_output));
+    gpuErrchk(cudaFree(device_input)); gpuErrchk(cudaFree(device_filter)); gpuErrchk(cudaFree(device_output));
 
     result.output = std::move(output);
     return std::move(result);
 }
 
 int main(int argc, char** argv){
-    int inputsize = 512;
-    std::vector<int> input(inputsize);
-    std::generate(input.begin(), input.end(), []() { static int x = -1; x++;return x; });
-    std::vector<int> filter{1,2,3,2,1};
+    //int inputsize = 1024*1024;
+    std::vector<int> input{1,4,2,5,235,234,1};//(inputsize);
+    //std::generate(input.begin(), input.end(), []() { static int x = -1; x++;return x; });
+    std::vector<int> filter{1,4,3,1};
+    //std::generate(filter.begin(), filter.end(), []() { static int x = -1; x++;return x; });
 
     Result r = CpuPerformConvolution(input, filter);
 
     for (ConvolutionCudaKernel cudakern : cudaKernels){
         Result r1 = CudaPerformConvolution(input, filter, cudakern);
         std::cout << "Kernel Executed in: " << r1.executiontime << " milliseconds" << std::endl;
-        for (auto element : r1.output)
-            std::cout << element << ", ";
+        //for (int i = 0; i < 10; i++)
+        //    std::cout << r1.output[i] << ", ";
+        //std::cout << "... ";
+        //for (int i = r1.output.size()-5; i < r1.output.size(); i++)
+        //    std::cout << r1.output[i] << ", ";
+        for (auto e : r1.output){
+            std::cout << e << ", ";
+        }
         std::cout << std::endl << std::endl;
     }
 }
