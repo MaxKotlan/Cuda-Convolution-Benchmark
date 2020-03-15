0c08589b199a97eea3a1cc5eeef3a22a15f3dea4
max <max.maxkot@gmail.com>
2020-03-15 11:01:39 -0400

    once filter reaches max constant memory size, set to fixed value. Continue to scale input.

--- Benchmark.cu
+++ Benchmark.cu
@@ -10,15 +10,13 @@ void Benchmark(){
     std::cout << "Starting Benchmark. Saving to KernelPerformance.csv" << std::endl;
     ofs.open ("KernelPerformance.csv", std::ofstream::out | std::ofstream::trunc);
     
-    for (int i = 1; i < (1<<10); i*=2 ){
+    for (int i = 1; i < (1<<25); i*=2 ){
         std::vector<int> input(i);//(inputsize);
         std::generate(input.begin(), input.end(), []() { return rand()%100; });
-        std::vector<int> filter(i/2);
+        int filtersize = i/2 < 0x990 ? i/2 : 0x990;
+        std::vector<int> filter(filtersize);
         std::generate(filter.begin(), filter.end(), []() { return rand()%100; });
-        if (i/2 < 0x990)
-            CsvPerformanceRow<int, 0x990>(ofs, true, input, filter);
-        else 
-        CsvPerformanceRow<int>(ofs, true, input, filter);
+        CsvPerformanceRow<int, 0x990>(ofs, true, input, filter);
     }
     
     ofs.close();
