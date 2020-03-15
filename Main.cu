a143ef4787b27f916cf579ee6d00aa99c41b0b8e
max <max.maxkot@gmail.com>
2020-03-15 10:36:37 -0400

    basic console

--- Main.cu
+++ Main.cu
@@ -2,10 +2,35 @@
 #include "Test.h"
 #include "Benchmark.h"
 
+bool enableconsole = true;
+
+void runCommand(std::string command){
+    std::cout << command << std::endl;
+    if (command == "test") RunTests(); else
+    if (command == "benchmark") Benchmark(); else
+    enableconsole = true;
+}
+
 int main(int argc, char** argv){
     for (int i = 0; i < argc; i++){
         std::string parameter = std::string(argv[i]);
-        if (parameter == "--test") RunTests();
-        if (parameter == "--benchmark") Benchmark();
+        int pos = parameter.find("--");
+        if (pos != std::string::npos){
+            std::string token = parameter.substr(pos+2, parameter.size()); 
+            runCommand(token);
+        }
     }
+    if (enableconsole){
+        std::cout << "Please enter a command. Current options are: "; std::cout << std::endl;
+        std::cout << "\ttest      - runs unit tests" << std::endl;
+        std::cout << "\tbenchmark - runs benchmark mode" << std::endl;
+        std::cout << "\tquit      - exits console" << std::endl;
+        std::cout << std::endl;
+        std::string x = "";
+        do{
+            std::cout << "Convolution Console: ";
+            runCommand(x);
+        } while ((std::cin >> x) && x != "quit");
+    }
+
 }
